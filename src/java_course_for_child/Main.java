3960ab46fce65edf37bd74b228d5657b30b1f61c
Ilnaz Daminov <ilnazdaminov@bk.ru>
2021-09-05 08:32:39 +0300

    lesson 1.3

--- src/java_course_for_child/Main.java
+++ src/java_course_for_child/Main.java
@@ -3,11 +3,21 @@ package java_course_for_child;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("  1  |  2  |  3  ");
+        String a1 = "1";
+        String a2 = "2";
+        String a3 = "3";
+        String a4 = "4";
+        String a5 = "5";
+        String a6 = "6";
+        String a7 = "7";
+        String a8 = "8";
+        String a9 = "9";
+
+        System.out.println("  "+a1+"  |  "+a2+"  |  "+a3+"  ");
         System.out.println("_____|_____|_____");
-        System.out.println("  4  |  5  |  6  ");
+        System.out.println("  "+a4+"  |  "+a5+"  |  "+a6+"  ");
         System.out.println("_____|_____|_____");
-        System.out.println("  7  |  8  |  9  ");
+        System.out.println("  "+a7+"  |  "+a8+"  |  "+a9+"  ");
         System.out.println("     |     |     ");
 
     }

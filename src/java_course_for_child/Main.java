39e48f9c6b690899177af2e93bfda3712e83b99e
Ilnaz Daminov <ilnazdaminov@bk.ru>
2021-09-05 08:23:33 +0300

    lesson 1.2

--- src/java_course_for_child/Main.java
+++ src/java_course_for_child/Main.java
@@ -2,6 +2,13 @@ package java_course_for_child;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello! My name is Ilnaz.");
+
+        System.out.println("  1  |  2  |  3  ");
+        System.out.println("_____|_____|_____");
+        System.out.println("  4  |  5  |  6  ");
+        System.out.println("_____|_____|_____");
+        System.out.println("  7  |  8  |  9  ");
+        System.out.println("     |     |     ");
+
     }
 }
